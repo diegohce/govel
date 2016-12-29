d57b9d8296438568dad930ca17f6ae7d0ad2e798
Diego Cena <gogs@fake.local>
2016-12-29 15:30:10 -0300

    dev commit

---
+++ src/receive.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+
+func main() {
+
+	conn, err := amqp.Dial("amqp://guest:guest@10.0.3.214:5672/")
+	failOnError(err, "Error connecting to RabbitMQ")
+	defer conn.Close()
+
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+
+	msgs, err := ch.Consume(
+	  "Q_test", //q.Name, // queue
+	  "",     // consumer
+	  true,   // auto-ack
+	  false,  // exclusive
+	  false,  // no-local
+	  false,  // no-wait
+	  nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+	  for d := range msgs {
+		log.Printf("Received a message: %s with RK %s", d.Body, d.RoutingKey)
+	  }
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+
+
+}
+
+
+
---
+++ src/send.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+
+func main() {
+
+	conn, err := amqp.Dial("amqp://guest:guest@10.0.3.214:5672/")
+	failOnError(err, "Error connecting to RabbitMQ")
+	defer conn.Close()
+
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+
+	body := "hello world!"
+	err = ch.Publish(
+		"E_test",     // exchange
+		"00Z8", //q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing {
+		  ContentType: "text/plain",
+		  Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+
+}
+
+
+
